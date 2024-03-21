7b3ece093b16d08e1fe689c903494a0b1abb9908
harshsaini0707 <harshsaini0172@gmail.com>
2024-03-21 11:31:40 +0530

    Instant Shop

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -163,20 +163,25 @@ function classNames(...classes) {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+            
+               <div className={classNames(
+                          'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )} onClick={()=>navigate("/")}>Home</div>
+                    
+                    <div className={classNames(
+                          'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )} onClick={()=>navigate("/shop")}>Shop</div>
+                    <div className={classNames(
+                          'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )} onClick={()=>navigate("/about")}>About Us</div>
+                    <div className={classNames(
+                         'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )} onClick={()=>navigate("/contact")}>Contact Us</div>
+                  
              
             </div>
           </Disclosure.Panel>
